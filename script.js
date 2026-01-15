40a34207df7f51573e23e2e4b385f236d988be89
David-Bodloza <davidbodloza97@gmail.com>
2026-01-15 14:15:02 +0200

    Phase 1: Mini Proj(Discipine Tracker) - Add reactions to user actions.

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-console.log("Script loaded successfully");
+// STATE
+let sessionStarted = false;
 
-const header = document.querySelector("header");
+// SELECT ELEMENTS(Bridgs between JS and HTML)
+const statusText = document.getElementById("status");
+const startBtn = document.getElementById("startBtn");
+const finishBtn = document.getElementById("finishBtn");
 
-header.addEventListener("click", function(){
-    alert("You are building descipline")
-})
+// EVENTS
+startBtn.addEventListener("click",startSession);
+finishBtn.addEventListener("click", finishSession);
+
+//Logic/Functions
+function startSession(){
+    sessionStarted = true;
+    statusText.textContent = "Status: Coding in progress";
+}
+
+function finishSession(){
+    if(!sessionStarted){
+        alert("You must start first.")
+        return;
+    }
+
+    statusText.textContent = "Status: Session Completed 🎯"
+}
+
