65267d65693c4b529761a1b30c5c9efd512e7904
David-Bodloza <davidbodloza97@gmail.com>
2026-01-15 12:47:58 +0200

    Phase 1: Basic Structure

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Script loaded successfully");
+
+const header = document.querySelector("header");
+
+header.addEventListener("click", function(){
+    alert("You are building descipline")
+})
